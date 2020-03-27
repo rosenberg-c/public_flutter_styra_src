5ea1cd3d421c0816100a229bd04dacc2ede68bbb
Christian <rosenberg-c>
2020-03-28 00:00:54 +0100

    [refactor] Update theme

--- lib/services/theme/theme_service.dart
+++ lib/services/theme/theme_service.dart
@@ -14,6 +14,10 @@ class ThemeService {
     _primaryColor = SwatchColor.c(mColor: _primaryMapColor);
   }
 
+  set customTheme(ThemeData themeData) {
+    _mThemeData = themeData;
+  }
+
   get _getMaterialThemeData {
     if (_mThemeData == null) {
       _mThemeData = MaterialFactory().materialFactory(
@@ -33,27 +37,31 @@ class ThemeService {
     return _primaryColor;
   }
 
+  set pColor(MaterialColor color) {
+    _primaryColor = color;
+  }
+
   Color get appBarTextBtnColor {
     return Colors.white70;
   }
 
   Color get appBarIconColor {
-    return pColor[900];
+    return pColor.shade900;
   }
 
   Color get bottomNavigationBarSelected {
-    return pColor[50];
+    return pColor.shade50;
   }
 
   Color get bottomNavigationBarUnSelected {
-    return pColor[900];
+    return pColor.shade900;
   }
 
   Color get bottomNavigationBarBackground {
-    return pColor[400];
+    return pColor.shade400;
   }
 
   Color get error {
-    return Colors.amber[900];
+    return Colors.amber.shade900;
   }
 }
